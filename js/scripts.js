b0e190dee4aa9c7f8124767016f7b9914e2bcf61
Haley Bristol and Taylor Grohs <hebristol@gmail.com>
2016-01-13 13:05:11 -0800

    Add for loop in place of forEach

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,14 @@ $(document).ready(function(){
   $("#blank form").submit(function(event){
     var blanks = ["person1", "person2", "animal", "verb", "noun", "exclamation"]
 
-    blanks.forEach(function(blank){
+    for(var index= 0; index < blanks.length; index++) {
+      var userInput = $("input#" + blanks[index]).val();
+      $("." + blanks[index]).text(userInput);
+    };
+    /*blanks.forEach(function(blank){
       var userInput = $("input#" + blank).val();
       $("." + blank).text(userInput);
-    });
+    })*/
 
 
     $("#story").show();
