96df132e84a4a9e790aea2196df99a3d7c6e9f59
Haley Bristol and Taylor Grohs <taylorgrohs@gmail.com>
2016-01-13 11:24:58 -0800

    Add forEach loop in jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,11 @@
 $(document).ready(function(){
-  $("form").submit(function(event){
-    var person1= $("input#person1").val();
-    var person2= $("input#person2").val();
-    var animal= $("input#animal").val();
-    var verb= $("input#verb").val();
-    var noun= $("input#noun").val();
-    var exclamation= $("input#exclamation").val();
+  $("#blank form").submit(function(event){
+    var blanks = ["person1", "person2", "animal", "verb", "noun", "exclamation"]
 
-    $(".person1").text(person1);
-    $(".person2").text(person2);
-    $(".animal").text(animal);
-    $(".verb").text(verb);
-    $(".noun").text(noun);
-    $(".exclamation").text(exclamation);
+    blanks.forEach(function(blank){
+      var userInput = $("input#" + blank).val();
+      $("." + blank).text(userInput);
+    });
 
 
     $("#story").show();
